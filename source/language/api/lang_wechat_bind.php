d2d6c0bf8b24667ffa4cf0aacd6a1a7937ec2b4a
aragakiyui <heng8050lee@foxmail.com>
2018-01-13 13:21:32 +0800

    add language package and table class

---
+++ source/language/api/lang_wechat_bind.php
@@ -0,0 +1,29 @@
+<?php
+if(!defined('IN_DISCUZ')) {
+    exit('Access Denied');
+}
+
+$lang = array(
+    'title' => '绑定模具论坛账号',
+    'username' => '用户名',
+    "username_placeholder" => '请输入您的用户名',
+    'name_empty_tips' => '用户名不能为空',
+    'name_wrong' => '含有敏感字符',
+    'password' => '密码',
+    'password_placeholder' => '请输入您的登陆密码',
+    'password_empty_tips' => '密码不能为空',
+    'bind' => '绑定',
+    'ICP' => '浙ICP备15037217号',
+    'mouldbbs' => '中国模具论坛',
+    'mouldcn' => '中国模具网',
+    'system_error_or_unfirendly_access' => '出错了，请联系论坛管理员',
+    'tips' => '请在中国模具论坛的微信公众号内进行绑定',
+    'username_not_register' => '用户名不存在',
+    'password_not_corret' => '密码输入错误，请重新输入',
+    'has_been_binded' => '该论坛账号已绑定公众号，无需重复绑定。',
+    'bind_ok' => '绑定成功',
+    'bind_failed' => '绑定失败',
+    'can_sign_in_wechat' => '关闭本页面，去公众号领取红包吧！',
+    'know' => '知道了',
+    'try_again' => '再试试',
+);
